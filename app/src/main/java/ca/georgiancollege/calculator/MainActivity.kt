547b99d290b214f8a9afcbfce50c2c4f9cb1bc1c
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-02 21:13:08 -0400

    Added internal documentation

--- app/src/main/java/ca/georgiancollege/calculator/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/calculator/MainActivity.kt
@@ -1,3 +1,13 @@
+/** Author: Basil Barnaby
+ * Student Number: 200540109
+ * Course: COMP3025 - Mobile and Pervasive Computing
+ * Assignment: 1 - Calculator App UX Design
+ * Date: June 2, 2024
+ * Description: This is a calculator app ux design that displays its elements in portrait and landscape.
+ * Target Device: Google Pixel 8 Pro
+ * Version: 0.1
+ */
+
 package ca.georgiancollege.calculator
 
 import android.os.Bundle
