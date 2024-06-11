102dccac01c638fd93a3bdbbceda3cf7edafc63a
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-11 10:31:07 -0400

    Enabled view binding

--- app/src/main/java/ca/georgiancollege/calculator/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/calculator/MainActivity.kt
@@ -1,12 +1,12 @@
 /** Author: Basil Barnaby
  * Student Number: 200540109
  * Course: COMP3025 - Mobile and Pervasive Computing
- * Assignment: 1 - Calculator App UX Design
- * Date: June 2, 2024
- * Description: This is a calculator app ux design that displays its elements in portrait and landscape.
+ * Assignment: 1 - Calculator App
+ * Date: June 16, 2024
+ * Description: This is a calculator app TODO: UPDATE HERE
  * Filename: Calculator App
  * Target Device: Google Pixel 8 Pro
- * Version: 0.1
+ * Version: 0.2.0
  */
 
 package ca.georgiancollege.calculator
@@ -16,10 +16,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ca.georgiancollege.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
