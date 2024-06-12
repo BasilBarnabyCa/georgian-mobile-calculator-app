a4226b7cadfd39e20289c05c2d62e9c7e266559e
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-12 12:49:19 -0400

    Set content view to binding and added tags for all buttons

--- app/src/main/java/ca/georgiancollege/calculator/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/calculator/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
