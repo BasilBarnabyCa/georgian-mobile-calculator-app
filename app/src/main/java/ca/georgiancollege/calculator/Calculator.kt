650f62b0391a158e6cf74e8ff281529f95cb78ed
Basil Barnaby <basilbarnaby.ca@gmail.com>
2024-06-13 14:59:58 -0400

    Refactored decimal handling by adding variable to track current number

--- app/src/main/java/ca/georgiancollege/calculator/Calculator.kt
+++ app/src/main/java/ca/georgiancollege/calculator/Calculator.kt
@@ -1,12 +1,15 @@
 package ca.georgiancollege.calculator
 
 import android.content.Context
+import android.util.Log
 import ca.georgiancollege.calculator.databinding.ActivityMainBinding
 
 class Calculator(dataBinding: ActivityMainBinding, context: Context) {
 
     private var binding: ActivityMainBinding = dataBinding
     private var formula: String
+    private var currentNumber: String
+    private var isCurrentNumber: Boolean
     private val operatorMap = mapOf(
         "plus" to context.getString(R.string.plus_text),
         "minus" to context.getString(R.string.minus_text),
@@ -15,7 +18,9 @@ class Calculator(dataBinding: ActivityMainBinding, context: Context) {
     )
 
     init {
-        formula = "0"
+        formula = ""
+        currentNumber = "0"
+        isCurrentNumber = true
         createButtons()
     }
 
@@ -32,7 +37,6 @@ class Calculator(dataBinding: ActivityMainBinding, context: Context) {
             binding.nineButton,
             binding.zeroButton,
             binding.decimalButton,
-//            binding.clearButton,
             binding.deleteButton,
             binding.plusMinusButton
         )
@@ -55,30 +59,38 @@ class Calculator(dataBinding: ActivityMainBinding, context: Context) {
     private fun attachOperand(tag: String) {
         when (tag) {
             "." -> {
-                // found a way to track decimals within each number
-
-                if (!binding.formulaTextView.text.contains(".")) {
-                    formula += if (formula.isEmpty() || !formula.last().isDigit()) "0." else "."
-                    binding.formulaTextView.text = formula
+                if (!currentNumber.contains(".")) {
+                    if (currentNumber.isEmpty()) {
+                        currentNumber = "0."
+                    } else {
+                        currentNumber += "."
+                    }
                 }
+
+                // TODO: REFACTOR REPEATING CODE
+                binding.formulaTextView.text = String.format("%s%s", formula, currentNumber)
             }
 
             "delete" -> {
-                formula = formula.dropLast(1)
-                binding.formulaTextView.text =
-                    if (formula.isEmpty() || formula == "-") "0" else formula
-                formula = if (formula.isEmpty()) "0" else formula
+                if (currentNumber.isNotEmpty()) {
+                    currentNumber = currentNumber.dropLast(1)
+                } else if (formula.isNotEmpty()) {
+                    formula = formula.dropLast(1)
+                }
+                binding.formulaTextView.text = if (formula.isEmpty()) "0" else formula
             }
 
             "plus_minus" -> {
-                if (formula.startsWith("-")) {
-                    formula = formula.substring(1)
+                if (currentNumber.startsWith("-")) {
+                    currentNumber = currentNumber.substring(1)
                 } else {
-                    if (formula.isNotEmpty() && formula != "0") {
-                        formula = "-".plus(formula)
+                    if (currentNumber.isNotEmpty() && currentNumber != "0") {
+                        currentNumber = "-$currentNumber"
                     }
                 }
-                binding.formulaTextView.text = formula
+
+                // TODO: REFACTOR REPEATING CODE
+                binding.formulaTextView.text = String.format("%s%s", formula, currentNumber)
             }
 
             "clear" -> {
@@ -86,19 +98,26 @@ class Calculator(dataBinding: ActivityMainBinding, context: Context) {
             }
 
             else -> {
-                if (binding.formulaTextView.text == "0") {
-                    formula = tag
-                    binding.formulaTextView.text = formula
+                if (currentNumber == "0") {
+                    currentNumber = tag
                 } else {
-                    formula += tag
-                    binding.formulaTextView.text = formula
+                    currentNumber += tag
                 }
+
+                // TODO: REFACTOR REPEATING CODE
+                binding.formulaTextView.text = String.format("%s%s", formula, currentNumber)
             }
         }
+        Log.i("currentNumber", currentNumber)
     }
 
     private fun attachOperator(tag: String) {
         if (tag == "plus" || tag == "minus" || tag == "multiply" || tag == "divide") {
+            if (currentNumber.isNotEmpty()) {
+                formula += currentNumber
+                currentNumber = ""
+            }
+
             if (formula.isEmpty() || !formula.last().isDigit() || formula.last() == '.') {
                 formula = formula.dropLast(1)
             }
@@ -116,8 +135,8 @@ class Calculator(dataBinding: ActivityMainBinding, context: Context) {
     }
 
     private fun clearScreen() {
-        formula = "0"
         formula = ""
+        currentNumber = "0"
         binding.formulaTextView.text = "0"
     }
 }
